3f71b6a1a1466c441730fd62f736396f855ae25c
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2021-12-07 08:38:15 +0100

    07: completed

---
+++ 07/src/Main.kt
@@ -0,0 +1,29 @@
+import java.io.File
+import kotlin.math.absoluteValue
+
+fun main() {
+	val input = File("input.txt")
+		.readLines()[0]
+		.split(",")
+		.map { it -> it.toLong() }
+
+	println("Part01: ${part01(input)}")
+	println("Part02: ${part02(input)}")
+}
+
+fun part01(input: List<Long>): Long {
+	val min = input.minOf { it }
+	val max = input.maxOf { it }
+
+	return (min..max).map { i -> input.sumOf { (it - i).absoluteValue } }
+	.minOf { it }
+}
+
+fun part02(input: List<Long>): Long {
+	val min = input.minOf { it }
+	val max = input.maxOf { it }
+
+	return (min..max).map { i ->
+		input.sumOf { (1..(it - i).absoluteValue).sum() }
+	}.minOf { it }
+}
