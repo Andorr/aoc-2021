a229bc1c400df7e994092bcce0eb0d052a378e23
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2021-12-12 20:20:50 +0100

    12: refactored

--- 12/src/Main.kt
+++ 12/src/Main.kt
@@ -1,43 +1,70 @@
 import java.io.File
 
+const val START = "start"
+const val END = "end"
+
 fun main() {
-	val input = parse("input.txt")
+	val input = mutableMapOf<String, MutableSet<String>>()
+	File("input.txt")
+		.readLines()
+		.map { it.split("-") }
+		.forEach {
+			input.getOrPut(it[0]) { mutableSetOf() }.add(it[1])
+			input.getOrPut(it[1]) { mutableSetOf() }.add(it[0])
+		}
 
 	println("Part01: ${part01(input)}")
 	println("Part02: ${part02(input)}")
+}
+
+fun part01(map: Map<String, Set<String>>): Long {
+	val queue = mutableListOf<String>()
+	val visited = mutableMapOf<String, Int>()
+
+	queue.add(START)
+
+	return dfs(map, queue, visited) { it, q, _ ->
+		it[0].isUpperCase() || it !in q
+	}
 }
 
 fun part02(map: Map<String, Set<String>>): Long {
 	val queue = mutableListOf<String>()
 	val visited = mutableMapOf<String, Int>()
 
-	queue.add("start")
+	queue.add(START)
 
-	return dfs2(map, queue, visited)
+	return dfs(map, queue, visited) { it, _, v ->
+		(it[0].isUpperCase() || !v.containsValue(2) || v.getOrDefault(it, 0) == 0)
+				&& it != START
+	}
 }
 
-fun dfs2(map: Map<String, Set<String>>, queue: MutableList<String>, visited: MutableMap<String, Int>): Long {
+fun dfs(
+	map: Map<String, Set<String>>,
+	queue: MutableList<String>,
+	visited: MutableMap<String, Int>,
+	pred: (it: String, queue: List<String>, visited: Map<String, Int>) -> Boolean
+): Long {
 	if(queue.isEmpty()) {
 		return 0L
 	}
 
 	val node = queue.last()
-	if(node == "end") {
-		// println(queue)
+	if(node == END) {
 		queue.removeLast()
 		return 1L
 	}
 
 	val neighbors = map[node]!!
 	val numPathsWithEnd = neighbors
-		.filter { it[0].isUpperCase() || !visited.containsValue(2) || (visited.containsValue(2) && visited.getOrDefault(it, 0) == 0)}
-		.filter { it != "start" }
+		.filter { pred(it, queue, visited) }
 		.sumOf {
 			queue.add(it)
 			if(it[0].isLowerCase()) {
 				visited[it] = visited.getOrDefault(it, 0) + 1
 			}
-			val sum = dfs2(map, queue, visited)
+			val sum = dfs(map, queue, visited, pred)
 			if(it[0].isLowerCase()) {
 				visited[it] = visited.getOrDefault(it, 0) - 1
 			}
@@ -46,54 +73,4 @@ fun dfs2(map: Map<String, Set<String>>, queue: MutableList<String>, visited: Mut
 
 	queue.removeLast()
 	return numPathsWithEnd
-}
-
-fun part01(map: Map<String, Set<String>>): Long {
-	val queue = mutableListOf<String>()
-
-	queue.add("start")
-
-	return dfs(map, queue)
-}
-
-fun dfs(map: Map<String, Set<String>>, queue: MutableList<String>): Long {
-	if(queue.isEmpty()) {
-		return 0L
-	}
-
-	val node = queue.last()
-	if(node == "end") {
-		queue.removeLast()
-		return 1L
-	}
-
-	val neighbors = map[node]!!
-	val numPathsWithEnd = neighbors
-		.filter { it[0].isUpperCase() || !(it in queue) }
-		.sumOf {
-			queue.add(it)
-			dfs(map, queue)
-		}
-
-	queue.removeLast()
-	return numPathsWithEnd
-}
-
-
-fun parse(fileName: String): Map<String, Set<String>> {
-	val map = mutableMapOf<String, MutableSet<String>>()
-	File(fileName)
-		.readLines()
-		.map { it.split("-") }
-		.forEach {
-			if(!map.containsKey(it[0])) {
-				map[it[0]] = mutableSetOf<String>()
-			}
-			if(!map.containsKey(it[1])) {
-				map[it[1]] = mutableSetOf<String>()
-			}
-			map[it[0]]?.add(it[1])
-			map[it[1]]?.add(it[0])
-		}
-	return map
 }
